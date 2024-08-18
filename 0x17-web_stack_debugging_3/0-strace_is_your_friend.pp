9c3d944d0c02d4077d707b4e6bdd3d008e8f7fa9
Iykemanuel <Igweemmanuel@gmail.com>
2024-08-18 19:21:02 +0000

    Fix error 500 with Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,12 +6,13 @@ file { '/var/www/html/wp-includes':
 # Ensure the file exists with the correct source path
 file { '/var/www/html/wp-includes/class-wp-locale.php':
   ensure => file,
-  source => '/var/www/html/wp-includes/class-wp-locale.php', # Update this path to the actual location of the file
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
 
 # Exec resource to fix WordPress
 exec { 'fix-wordpress':
   command => '/bin/true',
   onlyif  => '/usr/bin/test -f /var/www/html/wp-includes/class-wp-locale.php',
+  path    => ['/usr/bin', '/bin'],
 }
 
