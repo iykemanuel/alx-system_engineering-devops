d6e68fea22db449450845bcf64b4fe2cd17df6b1
Iykemanuel <Igweemmanuel@gmail.com>
2024-08-18 18:32:49 +0000

    Fixed the Fix for 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,11 @@
-# Ensure the directory exists
-file { '/var/www/html/wp-includes':
-  ensure => directory,
-}
-
-# Ensure the file exists with the correct source path
 file { '/var/www/html/wp-includes/class-wp-locale.php':
   ensure => file,
-  source => '/var/www/html/wp-includes/class-wp-locale.php', # Update this path to the actual location of the file
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
 
-# Exec resource to fix WordPress
 exec { 'fix-wordpress':
-  command => '/bin/true',
-  onlyif  => '/usr/bin/test -f /var/www/html/wp-includes/class-wp-locale.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
+  onlyif  => '/usr/bin/test -f /var/www/html/wp-settings.php',
 }
 
