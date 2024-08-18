0e477d0436703b37d104a85ffa8b189dadc7930b
Iykemanuel <Igweemmanuel@gmail.com>
2024-08-18 18:36:16 +0000

    Super fix 500 with Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,4 @@
-file { '/var/www/html/wp-includes/class-wp-locale.php':
-  ensure => file,
-  source => '/var/www/html/wp-includes/class-wp-locale.php',
-}
-
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/',
-  onlyif  => '/usr/bin/test -f /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
